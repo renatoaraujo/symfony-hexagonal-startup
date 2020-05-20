f6e3ce7fc03f7e12812ec0e87e14aa2a6f69d52f
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2020-05-20 16:47:16 +0200

    Starting the domain

---
+++ src/Application/Http/Api/CreateNewProductAction.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Http\Api;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class CreateNewProductAction
+{
+    private SerializerInterface $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public function __invoke(): JsonResponse
+    {
+        $serialized = $this->serializer->serialize([
+            'CALL' => self::class
+        ], 'json');
+        return new JsonResponse($serialized, JsonResponse::HTTP_OK);
+    }
+}
---
+++ src/Domain/Service/CreateNewProductHandler.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\Service;
+
+use App\Domain\Command\CreateNewProduct;
+
+final class CreateNewProductHandler
+{
+    public function handle(CreateNewProduct $command)
+    {
+    }
+}
