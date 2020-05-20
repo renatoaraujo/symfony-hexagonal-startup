2e114f85d2b633fac4736f9dff18d35c036d9063
Renato Rodrigues de Araujo <renato.araujo@mytoys.de>
2020-05-20 20:34:08 +0200

    Basic implementation of Name object

--- src/Domain/Name.php
+++ src/Domain/Name.php
@@ -5,4 +5,22 @@ namespace App\Domain;
 
 final class Name
 {
+    private string $name;
+
+    private function __construct()
+    {
+    }
+
+    public static function fromString(string $name): Name
+    {
+        $nameInstance = new self();
+        $nameInstance->name = $name;
+
+        return $nameInstance;
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
 }
