02f3916d4a8b90080506b65593b3d4c1e182d280
Renato Rodrigues de Araujo <renato.araujo@mytoys.de>
2020-05-20 21:29:38 +0200

    Test description object

---
+++ tests/Domain/DescriptionTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Domain;
+
+use App\Domain\Description;
+use App\Domain\Exception\InvalidDescriptionLengthException;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+use Faker;
+use function random_bytes;
+
+final class DescriptionTest extends TestCase
+{
+    private Generator $generator;
+
+    public function setUp(): void
+    {
+        $this->generator = Faker\Factory::create();
+    }
+
+    public function testItCanConstructDescription(): void
+    {
+        $descriptionText = $this->generator->text(140);
+        $description = Description::fromString($descriptionText);
+        $this->assertEquals($descriptionText, $description->__toString());
+    }
+
+    public function testItCantConstructWithLessThan10CharactersDescription(): void
+    {
+        $this->expectException(InvalidDescriptionLengthException::class);
+        $descriptionText = $this->generator->text(9);
+        Description::fromString($descriptionText);
+    }
+
+    public function testItCantConstructWithMoreThan140CharactersDescription(): void
+    {
+        $this->expectException(InvalidDescriptionLengthException::class);
+        $descriptionText = random_bytes(180);
+        Description::fromString($descriptionText);
+    }
+}
